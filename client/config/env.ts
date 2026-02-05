c8407e2a9fe858c93556e6205e884338dc47985a
matilda <matilda@lsware.com>
2026-02-05 17:45:36 +0900

    🐛 .gitignore에서 data.db 추가 및 env.ts에서 JWK를 Jwk로 수정

--- client/config/env.ts
+++ client/config/env.ts
@@ -1,9 +1,9 @@
 import 'dotenv/config'
-import type { JWK } from '@atproto/oauth-client-node'
+import type { Jwk } from '@atproto/oauth-client-node'
 
 interface Env {
   PUBLIC_URL: string
-  PRIVATE_KEYS?: JWK[]
+  PRIVATE_KEYS?: Jwk[]
   PORT: string
   PLC_URL: string
   PDS_URL: string
@@ -12,7 +12,7 @@ interface Env {
 export const env: Env = {
   PUBLIC_URL: process.env.PUBLIC_URL || '',
   PRIVATE_KEYS: process.env.PRIVATE_KEYS 
-    ? JSON.parse(process.env.PRIVATE_KEYS) as JWK[]
+    ? JSON.parse(process.env.PRIVATE_KEYS) as Jwk[]
     : undefined,
   PORT: process.env.PORT || '3001',
   PLC_URL: process.env.PLC_URL || 'https://plc.directory',
