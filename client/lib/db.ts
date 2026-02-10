90e8b9e087e3e80024b37f2dec831957fcda8e86
matilda <matilda@lsware.com>
2026-02-10 16:40:53 +0900

    ✨ 데이터베이스 마이그레이션 기능 추가

--- client/lib/db.ts
+++ client/lib/db.ts
@@ -1,4 +1,9 @@
-import { createDb } from '#/db'
+import { createDb, migrateToLatest } from '#/db'
 import path from 'path'
 
 export const db = createDb(path.join(process.cwd(), 'data.db'))
+
+migrateToLatest(db).catch((err) => {
+  console.error('Database migration failed:', err)
+  process.exit(1)
+})
